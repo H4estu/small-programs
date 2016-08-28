bbf924371284440fd043efa2bb7d8c9c2172890d
Zach Wallace <zach.wallace@live.com>
2016-08-27 17:26:39 -0700

    created directory structure and files

---
+++ cs261/hdr/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+struct student {
+	int id;
+	int score;
+};
+
+void fooA(int *iptr);
+void generate(struct student *students)
+struct student *allocate();
+	
+#endif
