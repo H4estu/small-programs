eefda7a330ff97925c3d9821d9de926e8966e904
Zach Wallace <wallacez@oregonstate.edu>
2016-09-01 09:18:47 -0700

    Created python directory

---
+++ python/exercises.py
@@ -0,0 +1,13 @@
+# 
+# exercises.py
+# Implement Python script exercise from assignment 1 of CS344 Summer 2015
+#
+
+
+def digitProd()
+
+def main():
+    digitProd()
+    
+if __name__ == '__main__':
+    main()
