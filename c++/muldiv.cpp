9489b1d017d58cbd6431f3e7827cae659409696a
Zach Wallace <zach.wallace@live.com>
2017-09-12 17:59:33 -0700

    Modularized muldiv program into multiple files.

--- c++/muldiv.cpp
+++ c++/muldiv.cpp
@@ -11,15 +11,7 @@
 #include <iostream>
 #include <iomanip>   // to set decimal precision
 
-
-/*
- * Store a single entry in both the multiplication and division tables.
- */
-struct muldiv_entry {
-    int mul;
-    float div;
-};
-
+#include "muldiv.hpp"
 
 /*
  * Allocate space for an n x n array of structs and fill with the 
@@ -94,41 +86,4 @@ void free_tables(struct muldiv_entry** tables, int n)
     
     delete[] tables;
     std::cout << "deleted table " << std::endl;
-}
-
-
-int main(int argc, char *argv[])
-{
-    int n;
-    muldiv_entry **muldiv_tables;
-        
-    // Command line argument checking.
-    if (argc > 2) {
-	std::cerr << "Too many command line arguments." << std::endl;
-	exit(-1);
-    } else if (argc < 2) {
-	std::cerr << "Too few command line arguments." << std::endl;
-	exit(-1);
-    }
-    if (!isdigit(*argv[1])) {
-	std::cerr << "Command line argument not an integer" << std::endl;
-	exit(-1);
-    }
-
-    n = atoi(argv[1]);
-    std::cout << "Integer: " << n << std::endl;
-    
-    // Generate tables
-    std::cout << "Generating tables..." << std::endl;
-    muldiv_tables = generate_tables(n);
-    
-    // Print tables
-    std::cout << "Printing tables..." << std::endl;
-    print_tables(muldiv_tables, n);
-    
-    // Free memory
-    std::cout << "Freeing memory..." << std::endl;
-    free_tables(muldiv_tables, n);
-
-    return 0;
 }
