09245f98741754b48052be3155fdfc6056444cd4
Zach Wallace <zach.wallace@live.com>
2017-09-16 22:33:08 -0700

    Adding directory for c programs

---
+++ c/sieve.c
@@ -0,0 +1,14 @@
+/* 
+ * sieve.c
+ * Implementation of the Sieve of Eratosthenes in C.
+ *
+ * Author: Z. Wallace
+ * Last edit: 16 Sept. 2017
+ */
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
